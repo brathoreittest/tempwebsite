c94d0cfdba90c02c43de6e3a0227dd9d0aaf3957
brathoreit <brathoreit@gmail.com>
2023-04-19 08:47:02 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import './css/Sidebar.css';
 import Header from './Header';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Sidebar from './components/Sidebar';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home'
 import About from './pages/About';
 import Contact from './pages/Contact';
 
 function App() {
-  return (    
+  return (
   <Router>
     <div className="container-fluid">
       <div className="row">
